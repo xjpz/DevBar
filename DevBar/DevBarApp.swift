a870a8847b639e581d2fba1b8031f32e5911635e
xjpz <xjpz@users.noreply.github.com>
2026-04-13 20:32:20 +0800

    Initial Commit

---
+++ DevBar/DevBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevBarApp.swift
+//  DevBar
+//
+//  Created by xjpz on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct DevBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevBarTests/DevBarTests.swift
@@ -0,0 +1,19 @@
+//
+//  DevBarTests.swift
+//  DevBarTests
+//
+//  Created by xjpz on 4/13/26.
+//
+
+import Testing
+@testable import DevBar
+
+struct DevBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
